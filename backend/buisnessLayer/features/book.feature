c90144d1870519ee270f70323dad922c1bf17ac6
Joan Miquel Espada Sabat <joanmi@espada.cat>
2017-06-28 14:09:41 +0200

    new test scenario for books

--- backend/buisnessLayer/features/book.feature
+++ backend/buisnessLayer/features/book.feature
@@ -4,7 +4,12 @@ Feature: Books
 	So that I can modify it
 
 	Scenario: Creating a book entity
-		Given I want to create a book
-		When I create new instance
-		Then I should get an empty book instance ready for use
+		Given I want to create a new book
+		When I create new instance with name Pepe, author Manolo, genre Love and publish date today
+		Then I should get a book instance ready for use
+
+	Scenario: Modifing a book entity
+		Given I have a book entity with name Pepe
+		When I update the name to Jose
+		Then I should get a book with name attribute Jose instead of Pepe
 
--- backend/buisnessLayer/features/step_definitions/book.js
+++ backend/buisnessLayer/features/step_definitions/book.js
@@ -9,21 +9,56 @@ defineSupportCode(function({Given, When, Then}) {
     //setWorldConstructor(CustomWorld)
     let book;
 
-  Given('I want to create a book', function(callback) {
+  Given(/^I want to create a new book$/, function() {
     
-    this.book = null;// new Book();
-    callback();
+    this.book = null;
+    //callback();
   });
 
-  When('I create new instance', function (callback) {
-    this.book = new Book();
-    this.book.name ='pepe';
-    callback();  
+  When(/^I create new instance with name (.*), author (.*), genre (.*) and publish date today$/, 
+        function (name, author,genre,callback) {
+
+            this.aux={ name: name, author:author,genre:genre, callback:callback  }
+
+            this.book = new Book()
+            this.book.name =name
+            this.book.author = author
+            this.book.genre = genre
+            this.book.publishdate = Date.now()
+            
+            callback()
+          });
+
+  Then(/^I should get a book instance ready for use$/, function () {
+    chai.expect(this.book.name).to.equal( this.aux.name);
+    chai.expect(this.book.author).to.equal( this.aux.author);
+    chai.expect(this.book.genre).to.equal( this.aux.genre);
+    
+    
+    
+  });
+});
+
+
+defineSupportCode(function({Given, When, Then}) {
+    //setWorldConstructor(CustomWorld)
+    let book;
+
+  Given(/^I have a book entity with name (.*)$/, function(name) {
+    
+    this.book = new Book()
+    this.book.name =name
+    //callback();
   });
 
-  Then('I should get an empty book instance ready for use', function (callback) {
-    chai.expect(this.book.name).to.equal('pepe');
-    //assert.equal(1,1,'ssdff')
-    callback();  
+  When(/^I update the name to (.*)$/, 
+        function (newname, callback) {
+            this.book.name =newname
+            callback()
+          });
+
+  Then(/^I should get a book with name attribute (.*) instead of (.*)$/, function (newname, oldame) {
+    chai.expect(this.book.name).to.equal( newname)
+    
   });
 });
